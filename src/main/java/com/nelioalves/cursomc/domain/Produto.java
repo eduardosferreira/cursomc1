021548e008e5f3b1b8c901cead9101d614ed331b
Eduardo da Silva Ferreira <eduardo.s.ferreira@gmail.com>
2018-10-18 20:27:21 -0300

    62. ItemPedido e ItemPedidoPK

--- src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.nelioalves.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
@@ -33,6 +37,17 @@ private static final long serialVersionUID = 1L;
 	
 	
 	private List<Categoria> categorias = new ArrayList<>();
+
+	@OneToMany(mappedBy="id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido item : itens) {
+			lista.add(item.getPedido());
+		}
+		return lista;
+	}
 	
 	public Produto() {
 		super();
@@ -109,6 +124,14 @@ private static final long serialVersionUID = 1L;
 		this.categorias = categorias;
 	}
 	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
